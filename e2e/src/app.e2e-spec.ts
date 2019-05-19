87d436188db3167f45339be56fa6bc6382ed71c2
wal0x <h.medwail@gmail.com>
2019-05-19 16:43:50 +0000

    delete e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,10 +8,10 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to NestedRoutesModulesTemplate!');
-  });
+  // it('should display welcome message', () => {
+  //   page.navigateTo();
+  //   expect(page.getTitleText()).toEqual('Welcome to NestedRoutesModulesTemplate!');
+  // });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
